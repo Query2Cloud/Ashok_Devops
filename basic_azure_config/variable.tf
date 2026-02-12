06a91d58f662053b45b947e3fe9520a7284a60fd
Ashok <ASHOKPRABHA123@GMAIL.COM>
2026-02-12 13:36:22 +0000

    project

---
+++ basic_azure_config/variable.tf
@@ -0,0 +1,11 @@
+variable "name" {
+    default = "abcd"
+    type = "String"
+    description = "name of resource group"
+}
+
+variable "location" {
+    default = "uksouth"
+    type = "string"
+    description = "region of resource group"  
+}
